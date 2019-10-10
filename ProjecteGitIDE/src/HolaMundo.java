cd58a29c192403e002e16f4cd0378a78b564a594
carloshg90 <carloshg90@github.com>
2019-10-10 12:23:44 +0200

    commit primera modificacio

--- ProjecteGitIDE/src/HolaMundo.java
+++ ProjecteGitIDE/src/HolaMundo.java
@@ -4,6 +4,7 @@ public class HolaMundo {
 	public static void main(String[] args) {
 		
 		System.out.println("Aquest és el meu projecte Java amb Eclipse i Git.");
+		System.out.println("Primera modificacio.");
 
 	}
 
